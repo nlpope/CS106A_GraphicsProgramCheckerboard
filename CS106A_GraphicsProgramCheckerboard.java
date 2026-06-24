7d9aea5b6d3851aea089f9f51ea6da864f9dcda5
Noah Pope <npope@tutanota.com>
2026-06-24 14:20:52 -0500

    adding files to new repo

---
+++ CS106A_GraphicsProgramCheckerboard.java
@@ -0,0 +1,14 @@
+/**
+ * File: CS106A_GraphicsProgramCheckerboard.java
+ * ----------------------------
+ * Generates a checkerboard pattern to the GCanvas.
+ */
+
+import acm.program.*;
+import acm.graphics.*;
+import java.awt.*;
+
+public class CS106A_GraphicsProgramCheckerboard extends GraphicsProgram
+{
+	
+}
